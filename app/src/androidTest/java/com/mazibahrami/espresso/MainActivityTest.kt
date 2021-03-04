69260671fbe103932bcddcc6216b72e4a69bf95e
Maziyar Bahramian <maziyar.bahramian@gmail.com>
2021-03-04 21:56:37 +0330

    test capture image and set to imageview

---
+++ app/src/androidTest/java/com/mazibahrami/espresso/MainActivityTest.kt
@@ -0,0 +1,59 @@
+package com.mazibahrami.espresso
+
+import android.app.Activity
+import android.app.Instrumentation
+import android.app.Instrumentation.*
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.os.Bundle
+import android.provider.MediaStore
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.intent.Intents.intending
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasAction
+import androidx.test.espresso.intent.rule.IntentsTestRule
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import com.mazibahrami.espresso.ImageViewHasDrawableMatcher.hasDrawable
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.not
+import org.junit.Assert.*
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4ClassRunner::class)
+class MainActivityTest {
+
+    @get: Rule
+    val intentsTestRule = IntentsTestRule(MainActivity::class.java)
+
+    @Test
+    fun test_cameraIntent_isBitmapSetToImageView() {
+        // GIVEN
+        val activityResult = createImageCaptureActivityResultStub()
+        val expectedIntent: Matcher<Intent> = hasAction(MediaStore.ACTION_IMAGE_CAPTURE)
+        intending(expectedIntent).respondWith(activityResult)
+
+        // Execute and Verify
+        onView(withId(R.id.image)).check(matches(not(hasDrawable())))
+        onView(withId(R.id.button_launch_camera)).perform(click())
+        intending(expectedIntent)
+        onView(withId(R.id.image)).check(matches(hasDrawable()))
+    }
+
+    private fun createImageCaptureActivityResultStub(): ActivityResult? {
+        val bundle = Bundle()
+        bundle.putParcelable(
+            KEY_IMAGE_DATA,
+            BitmapFactory.decodeResource(
+                intentsTestRule.activity.resources,
+                R.drawable.ic_launcher_background
+            )
+        )
+        val resultData = Intent()
+        resultData.putExtras(bundle)
+        return ActivityResult(Activity.RESULT_OK, resultData)
+    }
+}
--- app/src/main/java/com/mazibahrami/espresso/MainActivity.kt
+++ app/src/main/java/com/mazibahrami/espresso/MainActivity.kt
@@ -1,15 +1,58 @@
 package com.mazibahrami.espresso
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import com.mazibahrami.espresso.databinding.ActivityMainBinding
 
+const val REQUEST_IMAGE_CAPTURE = 1234
+const val KEY_IMAGE_DATA = "data"
+
 class MainActivity : AppCompatActivity() {
+
+    private val TAG: String = "AppDebug"
+
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.buttonLaunchCamera.setOnClickListener {
+            dispatchCameraIntent()
+        }
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            Log.d(TAG, "RESULT_OK")
+            when (requestCode) {
+
+                REQUEST_IMAGE_CAPTURE -> {
+                    Log.d(TAG, "REQUEST_IMAGE_CAPTURE detected.")
+                    data?.extras.let { extras ->
+                        if (extras == null || !extras.containsKey(KEY_IMAGE_DATA)) {
+                            return
+                        }
+                        val imageBitmap = extras[KEY_IMAGE_DATA] as Bitmap?
+                        binding.image.setImageBitmap(imageBitmap)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun dispatchCameraIntent() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+    }
+
 }
+
+
