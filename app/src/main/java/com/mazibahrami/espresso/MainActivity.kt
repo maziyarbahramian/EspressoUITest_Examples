bd80ef84c720029ae6c9f5af741897c206f84f7d
Maziyar Bahramian <maziyar.bahramian@gmail.com>
2021-03-04 15:51:02 +0330

    start

--- app/src/main/java/com/mazibahrami/espresso/MainActivity.kt
+++ app/src/main/java/com/mazibahrami/espresso/MainActivity.kt
@@ -1,15 +1,53 @@
 package com.mazibahrami.espresso
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import com.bumptech.glide.Glide
 import com.mazibahrami.espresso.databinding.ActivityMainBinding
 
+const val GALLERY_REQUEST_CODE = 1234
+
 class MainActivity : AppCompatActivity() {
+
+    private val TAG: String = "AppDebug"
+
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.buttonOpenGallery.setOnClickListener {
+            pickFromGallery()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            Log.d(TAG, "RESULT_OK")
+            when (requestCode) {
+
+                GALLERY_REQUEST_CODE -> {
+                    Log.d(TAG, "GALLERY_REQUEST_CODE detected.")
+                    data?.data?.let { uri ->
+                        Log.d(TAG, "URI: $uri")
+                        Glide.with(this)
+                            .load(uri)
+                            .into(binding.image)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun pickFromGallery() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(intent, GALLERY_REQUEST_CODE)
     }
 }
