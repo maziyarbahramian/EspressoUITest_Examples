13c26cfe596b42dcbe10ad32196214b51f17279b
Maziyar Bahramian <maziyar.bahramian@gmail.com>
2021-03-05 14:49:39 +0330

    start

--- app/src/main/java/com/mazibahrami/espresso/MainActivity.kt
+++ app/src/main/java/com/mazibahrami/espresso/MainActivity.kt
@@ -2,14 +2,41 @@ package com.mazibahrami.espresso
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.afollestad.materialdialogs.MaterialDialog
+import com.afollestad.materialdialogs.input.input
 import com.mazibahrami.espresso.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = "AppDebug"
+
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.buttonLaunchDialog.setOnClickListener {
+            showDialog()
+        }
+    }
+
+    private fun showDialog() {
+        MaterialDialog(this)
+            .show {
+                input(
+                    waitForPositiveButton = true,
+                    allowEmpty = false
+                ) { dialog, name ->
+                    setNameToTextView(name.toString())
+                }
+                title(R.string.text_enter_name)
+                positiveButton(R.string.text_ok)
+            }
+    }
+
+    private fun setNameToTextView(name: String) {
+        binding.textName.text = name
     }
 }
